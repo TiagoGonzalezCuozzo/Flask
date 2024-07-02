c0b9c1aa5719db85aa3e2bbcc533d02812f53943
TiagoGonzalezCuozzo <tiago.gonzalezcuozzo@student.dlh.lu>
2024-07-02 17:17:05 +0200

    end of the class

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -24,5 +24,11 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World!'
     
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
     return app
 app= create_app()
